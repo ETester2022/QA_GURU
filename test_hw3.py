579b5b4366ea88a14b94c0dfc4729cdafc01b795
e.shipitsyn <e.shipitsyn@avtomatika.kz>
2025-05-25 19:13:45 +0500

    Исправлены названия тестов

--- test_hw3.py
+++ test_hw3.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
-def test_search_link(driver):
+def test_search_positive(driver):
     """Тест появления текста yashaka/selene: User-oriented Web UI browser tests in Python - GitHub"""
     driver.find_element(By.ID, 'searchbox_input').send_keys('yashaka/selene')
     driver.find_element(By.ID, 'searchbox_input').send_keys(Keys.RETURN)
@@ -10,7 +10,7 @@ def test_search_link(driver):
                                '//*[text()="yashaka/selene: User-oriented Web UI browser tests in Python - GitHub"]')
     assert link is not None
 
-def test_search_text(driver):
+def test_search_negative(driver):
     """Тест появления текста рон-дон-дон"""
     driver.find_element(By.ID, 'searchbox_input').send_keys('yashaka/selene')
     driver.find_element(By.ID, 'searchbox_input').send_keys(Keys.RETURN)
