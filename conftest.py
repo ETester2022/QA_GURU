2855b9c96be22e9c7778526c79e3c5a9e58a9fcd
e.shipitsyn <e.shipitsyn@avtomatika.kz>
2025-06-10 13:21:00 +0500

    Добавлена ДЗ 7test_hw7)

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import pytest
+import  os
+from zipfile import ZipFile
+
 
 
 @pytest.fixture()
@@ -12,3 +15,18 @@ def driver():
     driver = webdriver.Chrome(options=chrome_options)
     yield driver
     driver.quit()
+
+@pytest.fixture()
+def create_archive():
+
+    files = [
+        "example.xlsx",
+        "example.pdf",
+        "example.csv"
+    ]
+
+    # Создаем архив и добавляем файлы
+    with ZipFile("sources/archive.zip", 'w') as zf:
+        for file in files:
+            add_file = os.path.join('sources', file)
+            zf.write(add_file, os.path.basename(add_file))
---
+++ test_hw7.py
@@ -0,0 +1,41 @@
+from zipfile import ZipFile
+from pypdf import PdfReader
+import io
+import pandas as pd
+import csv
+
+def test_read_pdf(create_archive):
+    with ZipFile("sources/archive.zip") as zf:
+        pdf_bytes = zf.read("example.pdf")
+        pdf_file = io.BytesIO(pdf_bytes)
+        reader = PdfReader(pdf_file)
+
+        lst_texts = [
+            "PDF test file",
+            "Document file type: PDF",
+            "Version: 1.0"
+        ]
+
+        for text in lst_texts:
+            assert text in reader.pages[0].extract_text()
+
+def test_read_xlsx(create_archive):
+    with ZipFile("sources/archive.zip") as zf:
+        xlsx_bytes = zf.read("example.xlsx")
+        xlsx_file = io.BytesIO(xlsx_bytes)
+        df = pd.read_excel(xlsx_file)
+        columns = df.columns
+
+        assert columns[0] == "XLSX test file"
+
+def test_read_csv(create_archive):
+    with ZipFile("sources/archive.zip") as zf:
+        csv_bytes = zf.read('example.csv').decode('utf-8')
+        csv_file = io.StringIO(csv_bytes)
+        reader = csv.DictReader(csv_file)
+        first_row = next(reader)
+        expected = {
+            '': '',
+            'CSV test file': ''
+        }
+        assert first_row == expected
