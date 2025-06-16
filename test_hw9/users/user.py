1e06efde62dae9d1cd1661a0a7faddcb077565df
e.shipitsyn <e.shipitsyn@avtomatika.kz>
2025-06-16 14:03:21 +0500

    ЧАСТЬ II (реализовать в бренче high-level-step-objects)

---
+++ test_hw9/users/user.py
@@ -0,0 +1,14 @@
+
+class User:
+    def __init__(self, first_name, last_name, user_email,
+                 phone, date, picture, subject, address, state, city):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.user_email = user_email
+        self.phone = phone
+        self.date = date
+        self.picture = picture
+        self.subject = subject
+        self.address = address
+        self.state = state
+        self.city = city
